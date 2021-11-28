9be072b19bf3f55c3fd8603b8825f9ea06975a80
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-11-28 18:20:59 +0000

    cleaning and setting up

--- main.cpp
+++ main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
-#include <vector>
-
-#include "Date.hpp"
-#include "Aviao.hpp"
-#include "Voo.hpp"
-
-using namespace std;
 
 int main(){
 
-    Date d("20-05-2010");
-    cout<<d<<endl;
-
-    Voo f(21368,d,12,"Lisboa","Porto");
-    vector<Voo> v = {f};
-
-    Aviao a("A13",213,v);
-
-    string str = a.getPlano()[0].getOrigem();
-    
-    cout<<str<<endl;
-
+    std::cout << "working";
 }
---
+++ src/Plane.cpp
@@ -0,0 +1,3 @@
+#include "../include/Plane.hpp"
+
+
