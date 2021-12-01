ea46e24de145fb216c04cfee64842467c8f0a0e6
tomaspm98 <85106282+tomaspm98@users.noreply.github.com>
2021-12-01 19:22:38 +0000

    Add files via upload

--- src/Service.cpp
+++ src/Service.cpp
@@ -10,13 +10,11 @@ Service::Service(char type, Date date, string employer) {
 char Service::getType() const {return this->type;}
 Date Service::getDate() const{ return this->date;}
 string Service::getEmployer() const {return this->employer;}
-//queue<Service> Service::getDoneServices() const {return this->doneServices;}
 
 //setters
 void Service::setType(char type) {this->type=type;}
 void Service::setDate(Date date) {this->date=date;}
 void Service::setEmployer(string employer) {this->employer=employer;}
-//void Service::setDoneServices(queue<Service> doneServices) {this->doneServices=doneServices;}
 
 
 
