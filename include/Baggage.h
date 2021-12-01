680b3df01ff7f93632e9974d7dac328d0be8e1b5
tomaspm98 <85106282+tomaspm98@users.noreply.github.com>
2021-12-01 19:20:31 +0000

    Add files via upload

--- include/Baggage.h
+++ include/Baggage.h
@@ -2,15 +2,24 @@
 #define BAGGAGE_H
 
 #include <queue>
-#include "../include/Passenger.h"
-
 
 class Baggage{
 private:
-
+   float weight;
+   float length;
+   float width;
+   float thickness;
 public:
-    Baggage() = default;
-    // Baggage(id);
+    Baggage(float weigth, float length, float width, float thickness);
+    void setWeight(float weight);
+    void setLength(float length);
+    void setWidth(float width);
+    void setThickness(float thickness);
+
+    float getWeight() const;
+    float getLength() const;
+    float getWidth() const;
+    float getThickness() const;
 
 };
 
