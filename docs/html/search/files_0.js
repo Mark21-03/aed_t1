d83d7ba4196d0e132fd00dd80ecf4e07a2540229
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-12-18 20:15:55 +0000

    deleting docs

--- docs/html/search/files_0.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['bagaggecheckin_2ecpp_0',['BagaggeCheckIn.cpp',['../_bagagge_check_in_8cpp.html',1,'']]],
-  ['bagaggecheckin_2eh_1',['BagaggeCheckIn.h',['../_bagagge_check_in_8h.html',1,'']]],
-  ['baggage_2eh_2',['Baggage.h',['../_baggage_8h.html',1,'']]],
-  ['baggagecheckinexceptions_2eh_3',['BaggageCheckInExceptions.h',['../_baggage_check_in_exceptions_8h.html',1,'']]],
-  ['bst_2eh_4',['BST.h',['../_b_s_t_8h.html',1,'']]]
-];
--- docs/html/search/pages_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['content_20moved_0',['Content Moved',['../md_lib_googletest_master_googlemock_docs__r_e_a_d_m_e.html',1,'(Global Namespace)'],['../md_lib_googletest_master_googletest_docs__r_e_a_d_m_e.html',1,'(Global Namespace)']]],
-  ['customization_20points_1',['Customization Points',['../md_lib_googletest_master_googlemock_include_gmock_internal_custom__r_e_a_d_m_e.html',1,'(Global Namespace)'],['../md_lib_googletest_master_googletest_include_gtest_internal_custom__r_e_a_d_m_e.html',1,'(Global Namespace)']]]
-];
