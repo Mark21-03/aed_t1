1b9dd69e5118576484b9105ba99b1151444f7c85
Marco <marco14lyoko@gmail.com>
2021-12-19 15:45:49 +0000

    Added doc to service and serviceManagement

--- include/Service.h
+++ include/Service.h
@@ -24,23 +24,94 @@ private:
     planePlate plane;
 
 public:
+
+    /**
+     * Default constructor
+     */
     Service()=default;
+
+    /**
+     * Constructs a service with a specified type, a date, the employee
+     * who did the service and the plate of the plane where the service happens
+     * @param type type of service (m | c | o) - maintenance, cleaning, other
+     * @param date date of the service
+     * @param employer who did the service
+     * @param plate which plane received the service
+     */
     Service(char type, Date date,const string& employer,string plate);
 
+    /**
+     * Defines new type for a service
+     * @param newType value to be set
+     * @return changed service object
+     */
     Service& setType(char newType);
+
+    /**
+     * Defines new date for a service
+     * @param newDate value to be set
+     * @return changed service object
+     */
     Service& setDate(Date newDate);
+
+    /**
+     * Defines new employee for a service
+     * @param newEmployer value to be set
+     * @return changed service object
+     */
     Service& setEmployer(const std::string& newEmployer);
+
+    /**
+     * Defines new planePlate for a service
+     * @param newPlane value to be set
+     * @return changed service object
+     */
     Service& setPlate(const planePlate& newPlane);
 
+    /**
+     * Gives type of service
+     * @return service's type
+     */
     ServiceType getType() const;
+
+    /**
+     * Gives date of service
+     * @return service's date
+     */
     Date getDate() const;
+
+    /**
+     * Gives employee who does a service
+     * @return service's employee
+     */
     string getEmployer() const;
+
+    /**
+     * Gives the planePlate of a service
+     * @return service's planePlate
+     */
     planePlate getPlane() const;
+
+    /**
+     * Gives a formatted service string
+     * @return formatted string
+     */
     std::string getInfo() const;
+
+    /**
+     * Gives service's employee name as a char[]
+     * @return service's employee name
+     */
     char* getEmployerNameC();
 
 };
 
+/**
+ * Allows for outputting a service to a ostream
+ * @param os output stream to be used
+ * @param s service to be outputted
+ * @return output stream (changed)
+ */
 inline std::ostream& operator<< (std::ostream& os, const Service& s) {
 
     os << s.getType() << " " << s.getDate().getDate();
@@ -58,6 +129,12 @@ inline std::ostream& operator<< (std::ostream& os, const Service& s) {
     return os;
 }
 
+/**
+ * Allows for inputting a service from a istream
+ * @param is input stream  to be used
+ * @param s service to be set
+ * @return input stream (changed)
+ */
 inline std::istream& operator>> (std::istream& is, Service& s) {
     std::string string1;
 
@@ -80,15 +157,36 @@ inline std::istream& operator>> (std::istream& is, Service& s) {
     return is;
 }
 
+
+/**
+ * Tests if two services are equal by testing equivalence between their
+ * type, date, employee and planePlate
+ * @param s1 left-hand service being compared
+ * @param s2 right-hand service being compared
+ * @return boolean result from comparison
+ */
 inline bool operator==(const Service &s1, const Service &s2){
     return (s1.getType() == s2.getType() && s1.getDate() == s2.getDate()
             && s1.getEmployer()== s2.getEmployer() && s1.getPlane() == s2.getPlane());
 }
 
+/**
+ * Tests if a service is lesser than another by comparing the service's dates
+ * @param s1  left-hand service being compared
+ * @param s2 right-hand service being compared
+ * @return boolean result from comparison
+ */
 inline bool operator<(const Service &s1, const Service &s2){
     return (s1.getDate()<s2.getDate());
 }
 
+/**
+ * Tests if two services are different by testing if at least one
+ * attribute is different between them
+ * @param s1 left-hand service being compared
+ * @param s2 right-hand service being compared
+ * @return boolean result from comparison
+ */
 inline bool operator!=(const Service &s1, const Service &s2){
     return !(s1 == s2);
 }
--- include/ServiceManagement.h
+++ include/ServiceManagement.h
@@ -14,23 +14,100 @@ private:
     std::string path;
 public:
 
+    /**
+     * Constructs a ServiceManagement, loading all services in given file path
+     * @param path file path where services are stored
+     */
     explicit ServiceManagement(const std::string& path);
 
+    /**
+     * Default ServiceManagement Constructor
+     */
     ServiceManagement() = default;
 
+    /**
+     * Sets the list of done services with a given one
+     * @param newDoneServices new list to be set
+     */
     void setDoneServices(list<Service> newDoneServices);
+
+    /**
+     * Sets the queue of To-do services with a given on
+     * @param newToDoServices new queue to be set
+     */
     void setToDoServices(queue<Service> newToDoServices);
+
+    /**
+     * Gives a copy of done service's list
+     * @return done service's list copy
+     */
     list<Service> getDoneServices() const;
+
+    /**
+     * Gives a copy of to-do service's queue
+     * @return to-do service's queue copy
+     */
     queue<Service> getToDoServices() const;
+
+    /**
+     * Adds a service to the end of done services list
+     * @param service service to be added
+     */
     void addDoneServices(Service service);
+
+    /**
+     * Adds a service to the to-do services queue taking into consideration
+     * the date of the service being added, inserting it in the correct position
+     * @param service service to be added
+     */
     void addToDoService(Service service);
+
+    /**
+     * Changes the order in the to-do queue, by changing the position
+     * of a service based on a new given date
+     * @param service service to be rescheduled
+     * @param newDate new rescheduled date to be set
+     */
     void changeTodoServicePriority(const Service& service, const Date& newDate);
+
+    /**
+     * Removes a given service from the to-do queue returning boolean it was deleted
+     * @param service service to be removed
+     * @return boolean true if it was found and deleted else false
+     */
     bool deleteTodoService(const Service &service);
+
+    /**
+     * Removes service at the front of to-do queue and adds it to the done services list
+     */
     void DoneLatestService();
+
+    /**
+     * Returns boolean telling if a given service is in the to-do queue
+     * @param service service to be searched
+     * @return boolean true if given service is in to-do queue
+     */
     bool findTodoService(const Service &service);
+
+    /**
+     * Shows done services from done service's list in the specified date range
+     * @param ostream1 ostream to be used
+     * @param min min data interval
+     * @param max max data interval
+     */
     void showDoneServicesFromRange(ostream &ostream1, const Date &min, const Date &max) const;
+
+    /**
+     * Shows to-do services from done service's list in the specified date range
+     * @param ostream1 ostream to be used
+     * @param min min data interval
+     * @param max max data interval
+     */
     void showToDoServicesFromRange(ostream &ostream1, const Date &min, const Date &max) const;
 
+    /**
+     * Saves to file the changes made to the to-do queue or done list
+     */
     void saveToFile() ;
 };
 
